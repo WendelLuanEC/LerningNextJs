214d7178b2be619faa9977f360841b02b8c5a496
wendelluan <wendelluan@alu.ufc.br>
2024-07-16 14:45:39 -0300

    Repetição 1

---
+++ exercicios/pages/render/repeticao1.jsx
@@ -0,0 +1,31 @@
+export default function repeticao1() {
+    const listaAprovados = [
+        "João",
+        "Maria",
+        "Carlos",
+        "Bia",
+        "Lucilene",
+        "Ana",
+    ]
+
+    function renderizarLista() {
+        return listaAprovados.map((nome, i) => <li key={i}>{nome}</li>)
+    }
+
+    return (
+        <ul>
+            {renderizarLista()}
+        </ul>
+    )
+
+    // function renderizarLista() {
+    //     const itens = []
+
+    //         for (let i = 0; i < listaAprovados.length; i++) {
+    //            itens.push(<li key={i}>{listaAprovados[i]}</li>)
+                
+    //         }
+        
+    //         return itens
+    // }
+}
---
+++ tabuleiro/pages/index.jsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+import Subdivisao from "@/components/Subdivisao";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Subdivisao/>
+      <Subdivisao preta/>
+    </div>
+  );
+}
